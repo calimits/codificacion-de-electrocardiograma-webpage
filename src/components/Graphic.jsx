bef0d3e993c5bde631b712f9944edae6bd0998ed
calimits <claudiayabrahammusic@gmail.com>
2024-12-18 01:04:47 +0100

    adding plotly and creating Graphic

---
+++ src/components/Graphic.jsx
@@ -0,0 +1,13 @@
+import Plotly from 'plotly.js-dist-min'
+import data from "../utils/db.json"
+
+
+export default function Graphic({GraphicId,title}){
+
+    return (
+        <>
+            <h3>{title}</h3>
+            <div id={GraphicId} className='graphic'></div>
+        </>
+    )
+}
