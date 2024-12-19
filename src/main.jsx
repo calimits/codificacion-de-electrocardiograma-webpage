629e4b63356474404ed589f0e5f08e6742c4d61e
calimits <claudiayabrahammusic@gmail.com>
2024-12-19 21:40:18 +0100

    route añadida

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter as Router, Routes, Route} from 'react-router'
+import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router'
 import AboutEcr from './components/AboutEcr.jsx'
 import SignalCoded from './components/SignalCoded.jsx'
 import NavigationBar from './components/NavigationBar.jsx'
 import Header from './components/Header.jsx'
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
       <Header></Header>
       <Routes>
-        <Route path="/" element={<SignalCoded/>}/>
+        <Route path="/" index element={<SignalCoded/>}/>
         <Route path="/about" element={<AboutEcr/>}/>
+        <Route path="/index.html" element={<Navigate to="/"/>}/>
         <Route path="*" element={<h1>Error 404 Not Found</h1>}/>
       </Routes>
     </Router>
