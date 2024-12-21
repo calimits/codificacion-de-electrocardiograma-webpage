5661cec51e069fbd45b1dd34b232057d5b9d45df
calimits <calimitsacd@gmail.com>
2024-12-21 03:36:43 +0100

    esto pincha ya

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -6,7 +6,7 @@ export default function NavigationBar(){
         <nav>
         <Link to="/codificacion-de-electrocardiograma-webpage" className="links b-red" >ECG</Link>
         <Link to="/codificacion-de-electrocardiograma-webpage/about" className="links b-blue" >Acerca</Link>
-        <a href="https://github.com/calimits/codificacion-de-electrocardiograma" target="_blank" className="links b-green" >Github</a>
+        <a href="https://github.com/calimits/codificacion-de-electrocardiograma-webpage" target="_blank" className="links b-green" >Github</a>
         </nav>
     );
 } 
